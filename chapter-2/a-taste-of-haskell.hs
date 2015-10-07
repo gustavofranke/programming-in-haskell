24a5df4b6bc57bf9ca21f793f468f5ebb061fee2
Gustavo Franke <gustavo@mindsandmachines.com>
2015-10-07 16:19:43 +0100

    Initial commit

---
+++ chapter-2/a-taste-of-haskell.hs
@@ -0,0 +1,10 @@
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum (x : xs) = x + mySum xs
+
+qsort :: Ord a => [a] -> [a]
+qsort[] = []
+qsort(x:xs) = qsort smaller ++ [x] ++ qsort larger
+              where
+                smaller = [a|a <- xs, a<= x]
+                larger = [b|b<-xs, b>x]
