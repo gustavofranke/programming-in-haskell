36b1111334e849bb61d784763875e23a0cc70a35
Gustavo Franke <gustavo@mindsandmachines.com>
2015-10-08 16:46:58 +0100

    add ch4

---
+++ chapter-4/new-from-old.hs
@@ -0,0 +1,11 @@
+isDigit :: Char -> Bool
+isDigit c = c >= '0' && c <= '9'
+
+myEven :: Integral a => a -> Bool
+myEven n = n `mod` 2 == 0
+
+mySplitAt :: Int -> [a] -> ([a], [a])
+mySplitAt n xs = (take n xs, drop n xs)
+
+recip :: Fractional a => a -> a
+recip n = 1/n
