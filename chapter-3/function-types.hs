8e7899a559448feb8cd494b717f2a710e666b561
Gustavo Franke <gustavo@mindsandmachines.com>
2015-10-08 15:50:41 +0100

    finish ch 3

--- chapter-3/function-types.hs
+++ chapter-3/function-types.hs
@@ -1,9 +1,12 @@
 add      :: (Int, Int) -> Int
 add(x, y) = x + y
 
--- Curried functions
+-- Curried function
 add'    :: Int -> (Int -> Int) 
 add' x y = x + y
 
 zeroto  :: Int -> [Int]
 zeroto n = [0..n]
+
+mult :: Int -> (Int -> (Int -> Int))
+mult x y z = x * y * z
