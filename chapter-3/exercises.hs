13b3e9e179a08453ec41c4b0e212228aaea62efb
Gustavo Franke <gustavo@mindsandmachines.com>
2015-10-12 16:56:08 +0100

    solved few exercises in ch3

--- chapter-3/exercises.hs
+++ chapter-3/exercises.hs
@@ -1,17 +1,17 @@
 -- 1. What are the types of the following values?
--- ['a', 'b', 'c']
--- ('a', 'b', 'c')
--- [(False , 'O'), (True, '1')]
--- ([False , True ], ['0', '1'])
--- [tail , init , reverse ]
+-- ['a', 'b', 'c'] :: [Char]
+-- ('a', 'b', 'c') :: (Char, Char, Char)
+-- [(False , 'O'), (True, '1')] :: [(Bool, Char)]
+-- ([False , True ], ['0', '1']) :: ([Bool], [Char])
+-- [tail , init , reverse ] :: [[a] -> [a]]
 
 -- 2. What are the types of the following functions?
-second xs = head (tail xs)
-swap (x , y) = (y, x)
-pair x y = (x , y)
-double x = x * 2
-palindrome xs = reverse xs == xs
-twice f x = f (f x )
+second xs = head (tail xs) -- second :: [a] -> a
+swap (x , y) = (y, x)      -- swap :: (t1, t) -> (t, t1)
+pair x y = (x , y)         -- pair :: t -> t1 -> (t, t1)
+double x = x * 2           -- double :: Num a => a -> a
+palindrome xs = reverse xs == xs -- palindrome :: Eq a => [a] -> Bool
+twice f x = f (f x )       -- twice :: (t -> t) -> t -> t
 -- Hint: take care to include the necessary class constraints if the functions
 -- are defined using overloaded operators.
 
