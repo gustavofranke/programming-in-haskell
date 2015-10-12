2e6f2dc2cbef90c9eeaec6db8f3e1985dca968ce
Gustavo Franke <franke.gustavo@gmail.com>
2015-10-12 22:04:15 +0100

    solve ch2 exercises

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Programming in Haskell
 ==================
 
 This is a repo with code examples from this [excellent book](http://www.amazon.com/Programming-Haskell-Graham-Hutton/dp/0521692695) that I'm using to start playing with the **Haskell** language.
+
+### Bit of troubleshooting
+When solving excercises, was having these **Exception: chapter-2/exercises.hs:21:1-31: Non-exhaustive patterns in function myLast**
+So I kicked off the repl using `ghci -fwarn-incomplete-patterns`, that gives you more meaningful warnings.
--- chapter-2/exercises.hs
+++ chapter-2/exercises.hs
@@ -17,6 +17,10 @@ n = a `div` length xs
 -- 4. Show how the library function last that selects the last element of a non-
 -- empty list could be defined in terms of the library functions introduced
 -- in this chapter. Can you think of another possible definition?
+myLast :: [Int] -> Int
+myLast as = head (reverse as)
 
 -- 5. Show how the library function init that removes the last element from
 -- a non-empty list could similarly be defined in two different ways.
+myInit :: [Int] -> [Int]
+myInit xs = reverse (tail (reverse xs))
